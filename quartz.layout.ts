7416284bd880ded75f7878caf2b214264c3acd18
ooleem-generator <swj7563@gmail.com>
2025-10-16 00:22:17 +0900

    vault backup: 2025-10-16 00:22:17

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,17 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: "giscus",
+      options: {
+        repo: "ooleem-generator/blog",
+        repoId: "R_kgDOQCY6Tw",
+        category: "Announcements",
+        categoryId: "DIC_kwDOQCY6T84CwrnO",
+      },
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
